12077fc37805e53da82b537df1b78ea281fa427d
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2020-01-13 23:52:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,8 +10,8 @@ def find_element_index(array, value_to_find)
   
   
 def find_max_value(array)
-  a = %w[array] 
-  Array.max(array)
+  
+  return Array.max(array)
 end
 
 def find_min_value(array)
